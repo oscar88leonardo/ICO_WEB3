2fe0df92d5a58e31db8ef0e05f199250c439b77f
oscar88leonardo <58998546+oscar88leonardo@users.noreply.github.com>
2022-08-02 13:42:19 -0500

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xaA7FA8a05c36c42157A5640bCd6447e2b043a0de";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
